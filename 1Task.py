869e96a9d4299336c2b4db4bb919804acbe346c1
Vard98 <79333815+Vard98@users.noreply.github.com>
2023-02-26 21:44:46 +0400

    Add files via upload

---
+++ 1Task.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+
+
+firefox_driver = webdriver.Firefox()
+firefox_driver.get("https://www.demoblaze.com/")
+firefox_driver.save_screenshot("C:\\Users\\USER\\Pictures\\Screenshots\\firefox_screen.png")
+firefox_driver.quit()
+edge_driver = webdriver.Edge()
+edge_driver.get("https://www.demoblaze.com/")
+edge_driver.save_screenshot("C:\\Users\\USER\\Pictures\\Screenshots\\edge_screen.png")
+edge_driver.quit()
---
+++ 2Task.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+firefox_driver = webdriver.Firefox()
+firefox_driver.get("https://www.demoblaze.com/")
+
+try:
+    header_element = firefox_driver.find_element(By.XPATH, '//*[@id="navbarExample"]/ul/li[1]/a')
+    print("Element is located:", header_element)
+except NoSuchElementException:
+    print("No Such Element")
+
+
+try:
+    header_element = firefox_driver.find_element(By.CLASS_NAME, "nav-link")
+    print("Element is located:", header_element)
+except NoSuchElementException:
+    print("No Such Element")
+
+try:
+    header_element = firefox_driver.find_element(By.LINK_TEXT, "About")
+    print("Element is located:", header_element)
+except NoSuchElementException:
+    print("No Such Element")
+
+firefox_driver.quit()
